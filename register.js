c8827783cebc4f87c74b1e87b0e6ab58ef6bc488
Ben19001 <benkupersmit@gmail.com>
2024-11-25 11:16:37 -0500

    Better security

--- register.js
+++ register.js
@@ -124,8 +124,8 @@ async function helper(transporter, email, registerToken) {
 
 async function sendToConfirmAccount(email, registerToken) {
     const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
         secure: false,
         auth: {
             user: process.env.EMAIL_USER,
--- routes/reset.js
+++ routes/reset.js
@@ -21,8 +21,8 @@ router.get("/", csrfProtection, (req, res) => {
 
 const sendEmail = async function(emailRecipient, token) {
     const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
         secure: false,
         auth: {
             user: process.env.EMAIL_USER,
